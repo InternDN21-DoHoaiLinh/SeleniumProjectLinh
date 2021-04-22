15d1a317a9654135c4ecf7a353a6ae96fbfe5065
InternDN21.01.01DoHoaiLinh <InternDN21.01.01@logigear.com>
2021-04-22 16:29:09 +0700

    Use Maven dependencies to download drivers. Read data from JSON file.

---
+++ src/main/java/helpers/BrowserHelpers.java
@@ -0,0 +1,51 @@
+package helpers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class BrowserHelpers {
+    private static WebDriver driver;
+    public static WebDriverWait myWaitVar;
+
+    public enum DriverType {CHROME, FIREFOX, EDGE}
+
+    public static void startBrowser(DriverType type) {
+        switch (type) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("You're in trouble!");
+                break;
+        }
+
+        driver.manage().window().maximize();
+        myWaitVar = new WebDriverWait(driver, 3);
+    }
+
+    public static void quitBrowser() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    public static WebDriver getWebDriver() {
+        return driver;
+    }
+}
