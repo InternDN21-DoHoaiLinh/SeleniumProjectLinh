0573d63fa472982deacff540147da233f9ae43a4
InternDN21.01.01DoHoaiLinh <InternDN21.01.01@logigear.com>
2021-05-04 10:17:27 +0700

    Update new codeÂ

--- src/main/java/helpers/Constant.java
+++ src/main/java/helpers/Constant.java
@@ -1,14 +1,8 @@
 package helpers;
 
 public class Constant {
-
-    public static final String RAILWAY_URL = "http://www.railway2.somee.com";
-    public static final String USERNAME = "lookins@gmail.com";
-    public static final String PASSWORD = "12345678";
-    public static final String LOGIN_ERROR_MESSAGE = "Invalid username or password. Please try again.";
-    public static final String PASSWORD_ERROR_MESSAGE = "You must specify a password.";
-    public static final String USERNAME_ERROR_MESSAGE = "You must specify a username.";
+    public static final String RAILWAY_URL = "";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
     public static final long DEFAULT_TIME_WAIT = 5;
-
-    public static final String INVALID_LOGIN_JSON_DATA_FILE_PATH = "src/test/resources/data/login/invalidLoginData.json";
 }
