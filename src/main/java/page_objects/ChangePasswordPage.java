8ee2bcd7bbd260fa8d536f3ec3fcfa0bfca028cf
InternDN21.01.01DoHoaiLinh <InternDN21.01.01@logigear.com>
2021-05-06 10:27:06 +0700

    Add TC03

--- src/main/java/page_objects/ChangePasswordPage.java
+++ src/main/java/page_objects/ChangePasswordPage.java
@@ -1,4 +1,27 @@
 package page_objects;
 
+import elements.Button;
+import elements.Label;
+import elements.TextBox;
+import helpers.ElementHelper;
+import org.openqa.selenium.By;
+
 public class ChangePasswordPage extends BasePage {
+    private final TextBox txtCurrentPassword = new TextBox(By.id("currentPassword"));
+    private final TextBox txtNewPassword = new TextBox(By.id("newPassword"));
+    private final TextBox txtConfirmPassword = new TextBox(By.id("confirmPassword"));
+    private final Button btnChangePassword = new Button(By.cssSelector(".form-actions>input[value='Change Password']"));
+    private final Label lblSuccessMessage = new Label(By.cssSelector("p[class='message success']"));
+
+    public void changePassword(String currentPassword, String newPassword, String confirmPassword) {
+        ElementHelper.scrollToView(txtCurrentPassword.findElement());
+        txtCurrentPassword.enterText(currentPassword);
+        txtNewPassword.enterText(newPassword);
+        txtConfirmPassword.enterText(confirmPassword);
+        btnChangePassword.click();
+    }
+
+    public String getSuccessMessage() {
+        return lblSuccessMessage.getText();
+    }
 }
---
+++ src/test/java/tests/TestChangePassword.java
@@ -0,0 +1,39 @@
+package tests;
+
+import common.Log;
+import helpers.DataHelper;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import page_objects.ChangePasswordPage;
+import page_objects.HomePage;
+import page_objects.LoginPage;
+import page_objects.RegisterPage;
+
+public class TestChangePassword extends TestBase {
+    HomePage homePage = new HomePage();
+    RegisterPage registerPage = new RegisterPage();
+    LoginPage loginPage = new LoginPage();
+    ChangePasswordPage changePasswordPage = new ChangePasswordPage();
+
+    @Test(description = "User can change password")
+    public void C09() {
+        Log.startTestCase("TC09");
+
+        Log.info("> Create a new account for password changing purpose");
+        homePage.goToRegister();
+        String email = DataHelper.getRandomEmail();
+        String password = DataHelper.getRandomPassword();
+        registerPage.register(email, password, password, DataHelper.getRandomPIDNumber());
+
+        Log.info("> Login");
+        registerPage.goToLogin();
+        loginPage.login(email, password);
+
+        Log.info("> Change password");
+        homePage.goToChangePassword();
+        changePasswordPage.changePassword(password, "dohoailinh199", "dohoailinh199");
+
+        Log.info("> Verify success message");
+        Assert.assertEquals(changePasswordPage.getSuccessMessage(), "Your password has been updated!", "Success message does not match!");
+    }
+}
