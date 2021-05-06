b9d71028d5a06e0a4eb65143bf80a36dfd8db261
InternDN21.01.01DoHoaiLinh <InternDN21.01.01@logigear.com>
2021-05-06 09:00:47 +0700

    add newlind at end of some files

--- src/main/java/helpers/Constant.java
+++ src/main/java/helpers/Constant.java
@@ -2,7 +2,7 @@ package helpers;
 
 public class Constant {
     public static final String RAILWAY_URL = "http://www.railway2.somee.com/";
-    public static final String USERNAME = "lookins@gmail.com";
+    public static final String USERNAME = "lookinss@gmail.com";
     public static final String PASSWORD = "12345678";
     public static final long DEFAULT_TIME_WAIT = 5;
     public static final int DEFAULT_PASSWORD_LENGTH = 8;
