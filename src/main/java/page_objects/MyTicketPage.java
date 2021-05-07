f2546ed0f7db017976e4cf71c93d6a7e85486457
InternDN21.01.01DoHoaiLinh <InternDN21.01.01@logigear.com>
2021-05-07 13:59:31 +0700

    Add TC16:User can cancel a ticket

--- src/main/java/page_objects/MyTicketPage.java
+++ src/main/java/page_objects/MyTicketPage.java
@@ -1,4 +1,16 @@
 package page_objects;
 
+import elements.Table;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class MyTicketPage extends BasePage {
+    private final Table tblMyTable = new Table(By.className("MyTable"));
+
+    public int getAmountOfRows() {
+        List<WebElement> rowNumber = tblMyTable.findElement().findElements(By.cssSelector(".OddRow, .EvenRow"));
+        return rowNumber.size() + 1;
+    }
 }
